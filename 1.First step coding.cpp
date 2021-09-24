0b98f59b8f4b87f0adff4a4f19a1e7e98958b8ec
enameeg <45296488+enameeg@users.noreply.github.com>
2021-09-25 03:36:11 +0900

    1.First step coding.cpp

---
+++ 1.First step coding.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){                        
+	cout<< "urjver: ";
+
+        int x = 5 ; //integer - Buhel too hamgiin ih hereglegddeg ene ni variable buyu huvisagchuud yum
+        int y = 20;
+        int urjver = x*y; 
+        cout<<urjver<<endl; 
+
+        int noogdvor = x/y ; 
+        cout<< "noogdvor: ";
+        cout<<noogdvor<<endl;
+
+        int yalgavar = x-y ;
+        cout<< "yalgavar: ";
+        cout<<yalgavar<<endl;
+
+        int niilber = x+y ;
+        cout<< "niilber: ";
+        cout<<niilber<<endl;
+    
+ return 0;
+
+}  
